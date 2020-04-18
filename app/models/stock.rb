77507922fc963561ed475c9b3a85b57aabc787ee
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-17 17:40:51 -0700

    Adding Lookup method to Stock model, adding API credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_publishable_token],
+                                  secret_token: Rails.application.credentials.iex_client[:sandbox_secret_token],
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+    client.price(ticker_symbol)
+  end
 end
