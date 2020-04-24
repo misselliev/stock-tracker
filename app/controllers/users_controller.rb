2a6a158644b5e5e5eabc23620d2f94ffd0088cff
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-24 16:36:20 -0700

    Rendering js response to search friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,25 @@ class UsersController < ApplicationController
   def my_friends
     @my_friends = current_user.friends
   end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+      if @friend
+        respond_to do |format|
+          format.js { render partial: 'users/friends_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Could not find user"
+          format.js { render partial: 'users/friends_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a name or email to search"
+        format.js { render partial: 'users/friends_result' }
+      end
+    end
+  end
 end
