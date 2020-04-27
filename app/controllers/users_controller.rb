935ef69fc7996d0dc50ed20576099fe780c64086
Elizabeth Villalejos <nekobanpaia@gmail.com>
2020-04-27 10:28:32 -0700

    Adding user profile show page, adding button functionality to add stocks that a friend is tracking to your portfolio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,17 @@
 
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
   def my_friends
     @my_friends = current_user.friends
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def search
@@ -19,13 +25,13 @@ class UsersController < ApplicationController
         end
       else
         respond_to do |format|
-          flash.now[:alert] = "Could not find user"
+          flash.now[:alert] = 'Could not find user'
           format.js { render partial: 'users/friends_result' }
         end
       end
     else
       respond_to do |format|
-        flash.now[:alert] = "Please enter a name or email to search"
+        flash.now[:alert] = 'Please enter a name or email to search'
         format.js { render partial: 'users/friends_result' }
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :user_stocks, only: %i[create destroy]
   resources :friendships, only: %i[create destroy]
+  resources :users, only: [:show]
   get 'users/my_portfolio'
-  devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
